51318095512039226bdecf35d2c873c36066899a
LeviGabriel867 <levideveloper373@gmail.com>
2025-06-12 21:19:21 -0300

    transição de páginas

--- frontEnd/chefControl/src/pages/formRequest/FormPage.jsx
+++ frontEnd/chefControl/src/pages/formRequest/FormPage.jsx
@@ -1,17 +1,28 @@
+import { useState } from "react";
 import HeaderPages from "../../components/header/HeaderPages";
 import { MdOutlineTableBar } from "react-icons/md";
 import { IoFastFoodOutline } from "react-icons/io5";
+import { FaCheckCircle } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import "./FormPage.css";
 
-
 function FormPage() {
-    
-const navigate = useNavigate();
+    const navigate = useNavigate();
+    const [pedidoEnviado, setPedidoEnviado] = useState(false);
 
-const handleTablesPage =() =>{
-    navigate("/tablesPage");
-}
+    const handleTablesPage = () => {
+        navigate("/tablesPage");
+    };
+
+    const chamarAnimação = (e) => {
+        e.preventDefault(); 
+        setPedidoEnviado(true);
+
+        setTimeout(() => {
+            setPedidoEnviado(false); 
+        }, 2000);
+    };
+
     return (
         <div className='ConteinerFormPage'>
             <HeaderPages h1Header={'ChefControl'} pHeader={"Pedidos"} />
@@ -25,8 +36,16 @@ const handleTablesPage =() =>{
                         <IoFastFoodOutline />
                         <input type="text" name="request" id="request" placeholder="Pedido" />
                     </div>
-                    <button type="submit" onClick={handleTablesPage}>Adicionar pedido</button>
+                    <button type="submit" onClick={chamarAnimação}>Adicionar pedido</button>
+                    <button type="submit" onClick={handleTablesPage}>Visualizar pedidos</button>
                 </form>
+
+                {pedidoEnviado && (
+                    <div className="mensagem-enviada">
+                        <FaCheckCircle className="icone-check" />
+                        Pedido Enviado!
+                    </div>
+                )}
             </div>
         </div>
     );
